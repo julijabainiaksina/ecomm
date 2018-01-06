ccedc51c2006e25ae284668c23a450751880aa2a
Ashley <zcabxli@ucl.ac.uk>
2018-01-06 13:57:05 +0000

    Ready for test
    1. One object per day not completed
    2. Insert into different collections not completed

--- tutorial/tutorial/items.py
+++ tutorial/tutorial/items.py
@@ -4,11 +4,42 @@
 #
 # See documentation in:
 # http://doc.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 
-
-class TutorialItem(scrapy.Item):
+class FarfetchProductItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
+    designer_name = scrapy.Field()
+
+    product_category = scrapy.Field()
+
+    product_original_price = scrapy.Field()
+
+    designer_style_id = scrapy.Field()
+
+    discount = scrapy.Field()
+
+    product_Price = scrapy.Field()
+
+    product_availability = scrapy.Field()
+
+    date = scrapy.Field()
+
+    product_stock_volume = scrapy.Field()
+
+    product_type = scrapy.Field()
+
+    product_description = scrapy.Field()
+
+    product_colour = scrapy.Field()
+
+    ffetch_id = scrapy.Field()
+
+    product_name = scrapy.Field()
+
+    no_product_on_sale = scrapy.Field()
+
     pass
+
+class FarfetchDesignerItem(scrapy.Item):
+    designer_brands = scrapy.Field()
+    date = scrapy.Field()
--- tutorial/tutorial/spiders/writeData.py
+++
@@ -1,4 +0,0 @@
-import datetime
-
-with open('dataInfor.txt', 'a') as outFile:
-    outFile.write('\n' + str(datetime.datetime.now()))
